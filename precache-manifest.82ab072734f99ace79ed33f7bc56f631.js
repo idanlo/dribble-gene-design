169e625141956c645a4c561a04777ce94bc2d5ac
idanlo <idan2110@gmail.com>
2019-04-14 00:13:54 +0300

    Updates

---
+++ precache-manifest.82ab072734f99ace79ed33f7bc56f631.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87b5c628af42a0173eda",
+    "url": "/dribble-gene-design/static/js/runtime~main.c0352185.js"
+  },
+  {
+    "revision": "d5875642dd846669a0e2",
+    "url": "/dribble-gene-design/static/js/main.5c000ce5.chunk.js"
+  },
+  {
+    "revision": "b4ab761685dc300f885e",
+    "url": "/dribble-gene-design/static/js/2.5d8c2036.chunk.js"
+  },
+  {
+    "revision": "d5875642dd846669a0e2",
+    "url": "/dribble-gene-design/static/css/main.d06658af.chunk.css"
+  },
+  {
+    "revision": "6eea5fc5560e627a275f29bcb85e3e58",
+    "url": "/dribble-gene-design/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.86bb69645a137ae8e31beffebbf1a339.js"
+  "/dribble-gene-design/precache-manifest.82ab072734f99ace79ed33f7bc56f631.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/dribble-gene-design/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
